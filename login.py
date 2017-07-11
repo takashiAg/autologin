60d94a3e542f94c03c96cb87b3e034148426b740
安藤亮介 <crawd4274@gmail.com>
2017-07-11 10:58:36 +0900

    パイソンを自動起動にすれば自動で接続可能に
    2nd commit

--- login.py
+++ login.py
@@ -1,23 +1,29 @@
 import urllib
 import urllib2
 import base64
-
+import time
+import socket
 username="s17701"
 password="19961001aN"
-basiclogin=base64.b64encode(username+password)
-try:
-    req=urllib2.Request("http://login.tokyo-ct.ac.jp/hello",headers={'Authorization': "Basic czE3NzAxOjE5OTYxMDAxYU4="})
-    res=urllib2.urlopen(req).read()
-    print(res)
-except:
-    print("error")
-flag=0
-while flag==0:
-    try:
-        url="https://secwall.tokyo-ct.ac.jp:6082/php/uid.php?vsys=1&url=http://wiki.ros.org%2fja"
-        data='inputStr=&escapeUser=%s&user=%s&passwd=%s&ok=Login'%(username,username,password)
-        res=urllib2.urlopen(url,data).read()
-        print(res)
-        flag=1
-    except:
-        print("error")
+basiclogin=base64.b64encode(username+":"+password)
+
+while 1:
+    if socket.gethostbyname(socket.gethostname())[0:4]=="172.":
+        try:
+            url="https://secwall.tokyo-ct.ac.jp:6082/php/uid.php?vsys=1&url=http://wiki.ros.org%2fja"
+            data='inputStr=&escapeUser=%s&user=%s&passwd=%s&ok=Login'%(username,username,password)
+            res=urllib2.urlopen(url,data,timeout=2).read()
+            print(res)
+            time.sleep(30.0)
+        except:
+            print("error")
+            time.sleep(1.0)
+    else:
+        try:
+            req=urllib2.Request("http://login.tokyo-ct.ac.jp/hello",headers={'Authorization': "Basic "+basiclogin})
+            res=urllib2.urlopen(req).read()
+            print(res)
+            time.sleep(25.0)
+        except:
+            print("error")
+            time.sleep(2.0)
